75a705413fecad33b526bf7a40447d33a488d76f
foophoof <me@foophoof.com>
2025-05-30 10:30:00 +0100

    wip 2

--- Tourist/Plugin.cs
+++ Tourist/Plugin.cs
@@ -1,25 +1,34 @@
 ﻿using Autofac;
-using DalaMock.Host.Hosting;
+using Autofac.Extensions.DependencyInjection;
+using DalaMock.Host.Factories;
+using DalaMock.Host.LoggingProviders;
+using DalaMock.Shared.Classes;
+using DalaMock.Shared.Interfaces;
+using Dalamud.Interface;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using FFXIVWeather.Lumina;
-using KamiToolKit;
 using Lumina;
 using Lumina.Excel;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
-using Tourist.Addons;
 using Tourist.Services;
 using Tourist.Windows;
 
 namespace Tourist;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
-public sealed class Plugin : HostedPlugin
+public sealed class Plugin : IDalamudPlugin
 {
+    private readonly IHost _host;
+    private readonly IPluginLog _pluginLog;
+    private readonly NativeUiService _nativeUiService;
+
     public Plugin(
         IDalamudPluginInterface pluginInterface,
         IPluginLog pluginLog,
@@ -29,18 +38,88 @@ public sealed class Plugin : HostedPlugin
         IGameGui gameGui,
         IFramework framework,
         IGameInteropProvider gameInteropProvider)
-        : base(
-            pluginInterface,
-            pluginLog,
-            dataManager,
-            commandManager,
-            clientState,
-            gameGui,
-            framework,
-            gameInteropProvider)
     {
-        CreateHost();
+        _nativeUiService = new NativeUiService(pluginInterface, framework, pluginLog);
+        _nativeUiService.Startup();
+
+        _pluginLog = pluginLog;
+
+        // _host = CreateHost();
+        // Start();
+
+        var hostBuilder = new HostBuilder()
+            .UseContentRoot(pluginInterface.ConfigDirectory.FullName)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureLogging(lb =>
+            {
+                lb.ClearProviders();
+                lb.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, DalamudLoggingProvider>(b =>
+                    new DalamudLoggingProvider(b.GetRequiredService<IPluginLog>())));
+                lb.SetMinimumLevel(LogLevel.Trace);
+            })
+            .ConfigureContainer<ContainerBuilder>(collection =>
+            {
+                collection.RegisterInstance(pluginInterface).As<IDalamudPluginInterface>().AsSelf();
+                collection.RegisterType<DalamudWindowSystem>().As<IWindowSystem>();
+                collection.RegisterType<WindowSystemFactory>().As<IWindowSystemFactory>().AsSelf().SingleInstance();
+
+                collection.RegisterInstance(pluginLog).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(dataManager).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(commandManager).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(clientState).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(gameGui).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(framework).AsSelf().AsImplementedInterfaces();
+                collection.RegisterInstance(gameInteropProvider).AsSelf().AsImplementedInterfaces();
+
+                collection.Register<IUiBuilder>(c =>
+                {
+                    var pluginInterface = c.Resolve<IDalamudPluginInterface>();
+                    return pluginInterface.UiBuilder;
+                });
+            });
+        hostBuilder.ConfigureContainer<ContainerBuilder>(ConfigureContainer);
+        _host = hostBuilder.Build();
+
         Start();
+    }
+
+    public bool IsStarted { get; private set; }
+
+    public void Start()
+    {
+        if (_host == null)
+        {
+            _pluginLog.Error("You attempted to start the plugin before the container has been built.");
+            return;
+        }
+
+        var startTask = _host.StartAsync();
+        if (startTask.IsFaulted)
+        {
+            _pluginLog.Error(startTask.Exception, "Plugin startup faulted.");
+            throw startTask.Exception;
+        }
+
+        IsStarted = true;
+    }
+
+    public void Dispose()
+    {
+        _pluginLog.Verbose("Plugin.Dispose, getting NativeUiService");
+        // var nativeUiService = _host.Services.GetService<NativeUiService>();
+
+        _pluginLog.Verbose("Plugin.Dispose, stopping host");
+        if (IsStarted)
+        {
+            _host.StopAsync().GetAwaiter().GetResult();
+            IsStarted = false;
+        }
+        _host.Dispose();
+
+        _pluginLog.Verbose("Plugin.Dispose, cleaning up NativeUiService");
+        _nativeUiService.Cleanup();
+
+        _pluginLog.Verbose("Plugin.Dispose, done");
     }
 
     private List<Type> HostedServices { get; } =
@@ -49,13 +128,12 @@ public sealed class Plugin : HostedPlugin
         typeof(ConfigurationLoaderService),
         typeof(InstallerWindowService),
         typeof(MarkerService),
-        typeof(NativeUiService),
         typeof(VfxService),
         typeof(VistaUnlockedListenerService),
         typeof(WindowService),
     ];
 
-    public override void ConfigureContainer(ContainerBuilder containerBuilder)
+    private void ConfigureContainer(ContainerBuilder containerBuilder)
     {
         foreach (var hostedService in HostedServices)
         {
@@ -66,15 +144,6 @@ public sealed class Plugin : HostedPlugin
         containerBuilder.Register(c => new FFXIVWeatherLuminaService(c.Resolve<GameData>()));
 
         containerBuilder.RegisterType<MainWindow>().As<Window>().AsSelf().SingleInstance();
-
-        // containerBuilder.RegisterType<NativeController>().AsSelf().SingleInstance();
-        // containerBuilder
-        //     .RegisterType<AddonTourist>()
-        //     .WithProperty("InternalName", "Tourist")
-        //     .WithProperty("Title", "Tourist")
-        //     .WithProperty("Size", new Vector2(350.0f, 450.0f))
-        //     .AsSelf()
-        //     .SingleInstance();
 
         // Sheets
         containerBuilder.RegisterGeneric((context, parameters) =>
@@ -93,7 +162,4 @@ public sealed class Plugin : HostedPlugin
             return configurationLoaderService.GetPluginConfig();
         }).SingleInstance();
     }
-
-    public override void ConfigureServices(IServiceCollection serviceCollection)
-    { }
 }
---
+++ Tourist/Services/NativeUiService.cs
@@ -0,0 +1,53 @@
+﻿using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Dalamud.Utility;
+using KamiToolKit;
+using System.Numerics;
+using Tourist.Addons;
+
+namespace Tourist.Services;
+
+public class NativeUiService(IDalamudPluginInterface pluginInterface, IFramework framework, IPluginLog pluginLog)
+{
+    private NativeController? _nativeController;
+    private AddonTourist? _addonTourist;
+
+    internal void Startup()
+    {
+        // ThreadSafety.AssertMainThread();
+        pluginLog.Verbose("Setting up Tourist NativeUiService");
+        _nativeController = new NativeController(pluginInterface);
+        _addonTourist = new AddonTourist(_nativeController)
+        {
+            InternalName = "Tourist",
+            Title = "Tourist",
+            Size = new Vector2(350.0f, 450.0f),
+        };
+        _addonTourist.Open(_nativeController);
+    }
+
+    // Must be called on framework thread
+    private void DisposeThings()
+    {
+        ThreadSafety.AssertMainThread();
+
+        pluginLog.Info("Disposing Native UI stuff, on main thread");
+        if (_addonTourist is null)
+        {
+            pluginLog.Verbose("_addonTourist is null");
+        }
+
+        if (_nativeController is null)
+        {
+            pluginLog.Verbose("_nativeController is null");
+        }
+        _addonTourist?.Dispose();
+        _nativeController?.Dispose();
+        pluginLog.Info("Done disposing Native UI stuff");
+    }
+
+    internal void Cleanup()
+    {
+        DisposeThings();
+    }
+}
